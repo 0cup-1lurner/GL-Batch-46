8ff71dd9f81d9eff432b76bbc9befeb558828aaf
Empty-Cup <neelalohitha.k@geeklurn.com>
2023-04-12 13:11:40 +0530

    Add employee_v3.py

---
+++ OOP/Employee_class/employee_v3.py
@@ -0,0 +1,19 @@
+
+class Employee:
+    def __init__(self):
+        self.name = "Virat"
+        self.age = 34
+        self.role = "Data Scientist"
+        self.salary = 1_000_000
+a = Employee()
+print(a.name, a.age, a.role, a.salary)
+class Employee:
+    def __init__(self):
+        self.name = "Rahul"
+        self.age = 32
+        self.role = "Data Engineer"
+        self.salary = 1_00_000
+
+e = Employee()  # Create a new Employee object
+                #(create an instance of the class Employee)
+print(e.name, e.age, e.role, e.salary)
