63519566520ba1f78ad58bca343e7bc22de612ec
Empty-Cup <neelalohitha.k@geeklurn.com>
2023-04-12 13:12:49 +0530

    Add employee_v7.py

---
+++ OOP/Employee_class/employee_v7.py
@@ -0,0 +1,14 @@
+
+class Employee:
+    def __init__(self, name, age, role, salary):
+        self.name = name
+        self.age = age
+        self.role = role
+        self.salary = salary
+
+    def increase_salary(self):
+        pass
+    
+a = Employee("Virat", 34, "Data Scientist", 1_000_000)
+
+
